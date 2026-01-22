c1bc89790d25ea94994b922b7f9aa6cb7009f2d1
darladavid <darladavid@darladavids-MacBook-Air.local>
2026-01-22 18:21:01 +0800

    added vue

---
+++ selfsnap-web/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-sky-200 flex items-center justify-center">
+    <div class="rounded-3xl bg-white p-8 shadow">
+      <h1 class="text-3xl font-black">Tailwind works ✅</h1>
+    </div>
+  </div>
+</template>
